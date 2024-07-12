565de4e349c18653c6ee6243312bacae14c227f8
Nikola <nikolanedeljkovic.official@gmail.com>
2024-07-12 03:04:20 +0200

    Initialize and Get Voice Connection

---
+++ src/utils/voiceConnection.ts
@@ -0,0 +1,33 @@
+import { entersState, getVoiceConnection, joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
+import { GuildBasedChannel } from "discord.js";
+
+async function initVoiceConnection(channel: GuildBasedChannel){
+    const connection = joinVoiceChannel({
+        guildId: channel.guildId,
+        channelId: channel.id,
+        adapterCreator: channel.guild.voiceAdapterCreator
+    })
+    connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+        try {
+            await Promise.race([
+                entersState(connection, VoiceConnectionStatus.Signalling, 2_000),
+                entersState(connection, VoiceConnectionStatus.Connecting, 2_000),
+            ]);
+            // Seems to be reconnecting to a new channel - ignore disconnect
+        } catch (err) {
+            connection.destroy();
+        }
+    });
+    return connection
+}
+
+async function getOrInitVoiceConnection(channel: GuildBasedChannel){
+    const oldConnection = getVoiceConnection(channel.guildId)
+    if(oldConnection) return oldConnection
+    return await initVoiceConnection(channel)
+}
+
+export {
+    getOrInitVoiceConnection,
+    initVoiceConnection
+}
