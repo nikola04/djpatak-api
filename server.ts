d2ce6cd37938efe65dc8ca7e1891bb39a5c64e21
Nikola <nikolanedeljkovic.official@gmail.com>
2024-07-12 02:59:06 +0200

    Initialize Clients and add Routes

--- server.ts
+++ server.ts
@@ -1,1 +1,45 @@
-console.log(process.env.TEST)
+import express from 'express'
+import mongoose from 'mongoose'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import playDl from 'play-dl'
+import v1Router from './src/api/v1/route'
+
+// INITIALIZATION
+const app = express()
+const botClient = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ] 
+})
+
+// ROUTES
+app.use('/api/v1/', v1Router)
+
+// EVENTS
+console.log("> Starting server...")
+app.listen(process.env.PORT, () => {
+    console.log('✅', `Listening on http://localhost:${process.env.PORT}/`)
+})
+
+botClient.once(Events.ClientReady, readyClient => {
+    console.log('✅', `Bot Ready! Logged in as ${readyClient.user.tag}!`);
+}).login(process.env.DISCORD_CLIENT_TOKEN)
+
+mongoose.connect(process.env.MONGODB_URI!).then(() => {
+    console.log('✅', 'MongoDB Connected!')
+}).catch((err) => {
+    console.error('❌', 'MongoDB Not Connected: ', err)
+})
+
+playDl.getFreeClientID().then((clientID: string) => {
+    playDl.setToken({
+      soundcloud : {
+          client_id : clientID
+      }
+    })
+})
+
+export { 
+    botClient
+}
